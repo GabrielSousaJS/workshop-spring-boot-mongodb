ad4ea7dfebcf58e341de795aaf3aaad3934c6c20
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-27 21:10:14 -0300

    FindById User

--- src/main/java/com/gabrielsousa/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielsousa/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.gabrielsousa.workshopmongo.services;
 
 import com.gabrielsousa.workshopmongo.domain.User;
 import com.gabrielsousa.workshopmongo.repositories.UserRepository;
+import com.gabrielsousa.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,4 +19,8 @@ public class UserService {
         return repository.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Not found user ID"));
+    }
 }
