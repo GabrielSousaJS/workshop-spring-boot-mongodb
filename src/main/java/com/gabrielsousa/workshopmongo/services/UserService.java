840713450b01bd8b849827991bb2fe1824b1a64a
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-27 23:50:37 -0300

    User insertion with POST

--- src/main/java/com/gabrielsousa/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielsousa/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.gabrielsousa.workshopmongo.services;
 
 import com.gabrielsousa.workshopmongo.domain.User;
+import com.gabrielsousa.workshopmongo.dto.UserDTO;
 import com.gabrielsousa.workshopmongo.repositories.UserRepository;
 import com.gabrielsousa.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Not found user ID"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        User obj = new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+        return obj;
     }
 }
