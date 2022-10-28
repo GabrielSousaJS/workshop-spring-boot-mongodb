ca5ff9e094aef243c22a4208c0a6136df45f64ac
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-28 11:52:50 -0300

    Creating entity user with nested post

---
+++ src/main/java/com/gabrielsousa/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielsousa.workshopmongo.repositories;
+
+import com.gabrielsousa.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
