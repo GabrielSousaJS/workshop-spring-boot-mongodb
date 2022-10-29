e4da737db2ff5494737facce41fcfc18f675f93d
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-28 23:21:09 -0300

    Getting a post by id

---
+++ src/main/java/com/gabrielsousa/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.gabrielsousa.workshopmongo.services;
+
+import com.gabrielsousa.workshopmongo.domain.Post;
+import com.gabrielsousa.workshopmongo.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
