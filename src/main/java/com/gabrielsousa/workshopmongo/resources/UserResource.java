5d97e96ab43f470343da4fb593e0d602fef55c81
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-28 23:11:26 -0300

    Endpoint to return a user's posts

--- src/main/java/com/gabrielsousa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielsousa/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.gabrielsousa.workshopmongo.resources;
 
+import com.gabrielsousa.workshopmongo.domain.Post;
 import com.gabrielsousa.workshopmongo.domain.User;
 import com.gabrielsousa.workshopmongo.dto.UserDTO;
 import com.gabrielsousa.workshopmongo.services.UserService;
@@ -53,5 +54,11 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable String id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
