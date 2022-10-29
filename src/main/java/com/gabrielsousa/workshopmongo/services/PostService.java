23d0ee844db80bc15dd4c469ed54d165c94ac45b
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-29 08:40:32 -0300

    Simple query with query methods

--- src/main/java/com/gabrielsousa/workshopmongo/services/PostService.java
+++ src/main/java/com/gabrielsousa/workshopmongo/services/PostService.java
@@ -5,6 +5,7 @@ import com.gabrielsousa.workshopmongo.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,4 +19,7 @@ public class PostService {
         return obj.get();
     }
 
+    public List<Post> findyByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
 }
